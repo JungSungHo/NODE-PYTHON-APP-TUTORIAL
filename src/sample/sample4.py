d03c76a004feea907a73cad3bb3d9c1fafb987aa
JUNG SUNGHO <jungsh@ais-info.co.jp>
2023-05-16 15:44:29 +0900

    add to tutorial

---
+++ src/sample/sample4.py
@@ -0,0 +1,19 @@
+import json
+
+data = {
+    "olivia" : {
+        "gender": "female",
+        "age" : 25,
+        "hobby" : ["reading", "music"]
+    },
+    "Tyler" : {
+        "gender": "male",
+        "age" : 28,
+        "hobby" : ["development", "painting"]
+    }
+}
+
+file_path = "./test.json"
+
+with open(file_path, 'w', encoding='utf-8') as file:
+    json.dump(data, file)
