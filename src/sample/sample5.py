a8a9a090978733474384537394e33fa79cb1904d
JUNG SUNGHO <jungsh@ais-info.co.jp>
2023-05-30 08:55:29 +0900

    add sample code

---
+++ src/sample/sample5.py
@@ -0,0 +1,32 @@
+print('Test1\newline')
+print('Test2\\aa')
+print('Test3\'aa')
+print('Test4\"aa')
+print('Test5\a')
+print('Test6\b')
+print('Test7\f')
+print('Test8\n')
+print('Test9\r')
+print('Test10\t')
+print('Test11\v')
+
+
+print("""\nTest1234""")
+print(r"Test2345")
+
+print(r"Test3456" * 4)
+
+# print('00\xhh')
+# print('Test14\N{Test}')
+# print('333\uxxxx')
+# print('444\Uxxxxxxxx')
+
+print("I'm A Boy")
+print("Hello".lower())
+print("Hello".upper())
+print("hello python".capitalize())
+print("MY FAVORITE THINGS".capitalize())
+print("hello python".title())
+print("Hello".swapcase())
+
+# Link: https://www.javadrive.jp/python/string/index12.html
